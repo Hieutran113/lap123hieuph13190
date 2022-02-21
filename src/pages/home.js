12993e2ca4408a1f1dfc736d12128182ce339945
hieutran <hieutdph13190@fpt.edu.vn>
2022-02-21 12:59:19 +0700

    Update home.js

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,41 +1,31 @@
-import data from "../data";
 
+import Header from "../components/header";
+import Banner from "../components/banner";
+import NewsList from "../components/newslist";
+import Footer from "../components/footer";
 const HomePage = {
-    render() {
+   async render() {
         return /* html */`
-        <div class="max-w-5xl mx-auto"> 
-            <div class="my-3">
-                <img src="https://picsum.photos/1024/400" />
-            </div>
-            <div class="news test">
-                <h2 class="text-2xl font-semibold my-4">Tin Tức Học Tập</h2>
-                <div class="grid grid-cols-3 gap-8">
-                    ${data.map((post) => `
-                        <div class="border p-4">
-                            <a href="/news/${post.id}">
-                                <img src="${post.img}" alt="" />
-                            </a>
-                            <h3 class="my-3"><a  href="/news/${post.id}"class="font-semibold text-lg text-orange-500">${post.title}</a></h3>
-                            <p>${post.desc}</p>
-                        </div>
-                    `).join("")}
+        <div class="header">
+            ${Header.render()}
+            <main>
+                <div class="banner">
+                    ${Banner.render()}
                 </div>
-            </div>
-            <div class="news">
-                <h2 class="text-2xl font-semibold my-4"> Hoạt Động Sinh Viên</h2>
-                <div class="grid grid-cols-3 gap-8">
-                    ${data.map((post) => `
-                        <div class="border p-4">
-                            <a href="/news/${post.id}">
-                                <img src="${post.img}" alt="" />
-                            </a>
-                            <h3 class="my-3"><a  href="/news/${post.id}"class="font-semibold text-lg text-orange-500">${post.title}</a></h3>
-                            <p>${post.desc}</p>
-                        </div>
-                    `).join("")}
+                <div class="content">
+                    ${await NewsList.render()}
                 </div>
-            </div>
+                <div>
+                    ${Footer.render()}
+                <div>
+            </main>
         </div>
+
+
+
+
+        
+        
         `;
     },
 };
